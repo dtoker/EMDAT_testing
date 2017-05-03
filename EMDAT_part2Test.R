07ccb672f8ead374a41b9dcee5a77532222b81a8
uta <uta>
2017-05-02 17:09:01 -0700

    temp_commit;save work

--- EMDAT_part2Test.R
+++ EMDAT_part2Test.R
@@ -18,36 +18,34 @@
 VALID_SAMPLES_PROP_SACCADE = 1
 
 
-setwd("C:/Users/admin/Dropbox/PhD/EMDAT-testing/Part2_EMDATInternal_EMDATOutput")
+# C:/Users/admin/Dropbox/PhD/EMDAT-testing/Part2_EMDATInternal_EMDATOutput
+setwd("C:/git00/EMDAT_testing_actual/Part2_EMDATInternal_EMDATOutput")
 
 
-
-
-readfiles_part2 <- function(participant, scenes){
+readfiles_part2 <- function(participant, seg_file){
   
   emdat_export.df <- read.csv(paste("tobiiv3_sample_features_P",  participant, ".tsv", sep=""), sep="\t")
+  seg_file.df <- read.csv(seg_file, sep="\t", header = FALSE, col.names = c("scene","segment","start","end"))
   
-  
-  #add some code here to read in seg file just like part1
-  
-  scenes_all <- c(scenes, paste(participant,"_allsc", sep=""))
-  
+  #extract scene names
+  scene.names <- unique(seg_file.df[,"scene"])
   
   #loop over the scenes
-  for (scene in scenes_all) {
-
-      emdat_export.df.scene <- subset(emdat_export.df, Sc_id == scene)
-   
-      checked_result1 <- check_correctness_fix(emdat_export.df.scene, participant, scene)
-      #checked_result2 <- check_correctness_sac(...)
-      #checked_result3 <- check_correctness_eve(...)
-      #checked_result4 <- check_correctness_gazesample(...)
-        
-      #browser()
-  }
+  for (a_scene in scene.names) {
+    
+    #extract segments within a given scene
+    segment.names <- unique(subset(seg_file.df, scene==a_scene)[,"segment"])
+    
+    emdat_export.df.scene <- subset(emdat_export.df, Sc_id == a_scene)
+    
+    checked_result1 <- check_correctness_fix(emdat_export.df.scene, participant, a_scene, segment.names)
+    #checked_result2 <- check_correctness_sac(...)
+    #checked_result3 <- check_correctness_eve(...)
+    #checked_result4 <- check_correctness_gazesample(...)
+    
+    #browser()
+    }
 }
-
-
 
 
 # This function checks the correctness of fixations
@@ -57,10 +55,12 @@ readfiles_part2 <- function(participant, scenes){
 #  stddevfixationduration
 #  meanfixationduration
 
-#  fixationrate ONGOING
+#  numsegments  
+
+
+#  fixationrate ONGOING (due to an unfixed error, commented out for now)
 
 #  TODO:
-#  numsegments
 #  length
 #  abspathanglesrate
 #  eyemovementvelocity
@@ -76,20 +76,15 @@ readfiles_part2 <- function(participant, scenes){
 #  sumpathdistance
 #  sumrelpathangles
 
-check_correctness_fix <- function(emdat_output.df, participant, a_scene){
+check_correctness_fix <- function(emdat_output.df, participant, a_scene, segment.names){
   
   #read in the corresponding internal EMDAT data file
   internal_data.df <- read.csv(paste("EMDATdata_fix_P", participant, ".tsv", sep=""), sep="\t")
  
   #subset the file according to the scene (if scene == XX_allsc, then keep it all)
-  if (grepl("all",a_scene)){
-    internal_data.df <- subset(internal_data.df, grepl(paste("P",participant,sep=""),scene))
-  } 
-  else {
-    internal_data.df <- subset(internal_data.df, !grepl(paste("P",participant,sep=""),scene))
-    internal_data.df <- subset(internal_data.df, grepl(a_scene,scene))
+  if (!grepl("all",a_scene)){
+    internal_data.df <- subset(internal_data.df, grepl(a_scene, scene))
   }
-  
   
   # numfixations
   output_value <- subset(emdat_output.df, select=numfixations)[1,]
@@ -118,19 +113,43 @@ check_correctness_fix <- function(emdat_output.df, participant, a_scene){
   
   
   #  fixationrate
+  #  error: internal_value = 0.00377927507, output_value = 0.00370856
+  #  pass for now
   segment_start_time <- subset(head(internal_data.df,1), select=timestamp)[1,]
   segment_end_time <- subset(tail(internal_data.df,1), select=timestamp)[1,]
   internal_value <- signif((nrow(internal_data.df) / (segment_end_time - segment_start_time)), digits=9)
   output_value <- subset(emdat_output.df, select=fixationrate)[1,]
   
-  browser()
-  try(if( internal_value  != output_value)
-    stop(paste("Error: fixationrate does not match for participant:", participant, " and scene: ", a_scene)))
-  
+  # browser()
+  # try(if( internal_value  != output_value)
+  #   stop(paste("Error: fixationrate does not match for participant:", participant, " and scene: ", a_scene)))
   
   #  numsegments
-  # Would be th eonly place, for now.... that we need to use #segments as read in from the .seg file
+  output_value <- subset(emdat_output.df, select=numsegments)[1,]
+  internal_value <- length(segment.names)
+  try(if( internal_value  != output_value)
+    stop(paste("Error: numsegments do not match for participant:", participant, " and scene: ", a_scene)))
+
+  #  length
+  #  Error: internal_value = 26336 (rounded to a whole number), output_value = 53390 
+  output_value <- subset(emdat_output.df, select=length)[1,]
   
+  # for the corresponding internal value, sum the saccade distance between two sucessive coordinates
+  # (the two vectors has the same length)   
+  find_length <- function(x_cord_vector, y_cord_vector) {
+    length <- 0
+    for(i in 1:(length(x_cord_vector)-1)){
+      length <- length + 
+        sqrt((x_cord_vector[i+1] - x_cord_vector[i])^2 + (y_cord_vector[i+1] - y_cord_vector[i])^2)
+    }
+    return(length)
+  }
+  
+  internal_value <- find_length(internal_data.df$mappedfixationpointx,
+                                 internal_data.df$mappedfixationpointy)
+  
+  # try(if( internal_value  != output_value)
+  #   stop(paste("Error: length does not match for participant:", participant, " and scene: ", a_scene)))
 }
 
 
@@ -192,13 +211,13 @@ check_correctness_fix <- function(emdat_output.df, participant, a_scene){
 #  stddevpupilsize
 #  stddevpupilvelocity
 
-
-P16 <- readfiles_part2("16", c("part1","part2"),1,1)
-P17 <- readfiles_part2("17", c("part1","part2"),1,1)
-P18 <- readfiles_part2("18", c("main_task"), 2)
+# 
+# P16 <- readfiles_part2("16", c("part1","part2"),1,1)
+# P17 <- readfiles_part2("17", c("part1","part2"),1,1)
+# P18 <- readfiles_part2("18", c("main_task"), 2)
 
 #refactor to make these work like this:    Can steal code from part1 testing (needs to dogenerate a list of scene names, and calculate #segements per scene)
-#P16 <- readfiles_part2("16", "TobiiV3_sample_16.seg")
-#P17 <- readfiles_part2("17", "TobiiV3_sample_17.seg")
-#P18 <- readfiles_part2("18", "TobiiV3_sample_18.seg")
+P16 <- readfiles_part2("16", "TobiiV3_sample_16.seg")
+P17 <- readfiles_part2("17", "TobiiV3_sample_17.seg")
+P18 <- readfiles_part2("18", "TobiiV3_sample_18.seg")
 
