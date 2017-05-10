71c2270134600a768265fda7f5b09d165168147c
uta <uta>
2017-05-09 20:17:58 -0700

    Delete unneccesary functions

--- EMDAT_test_utils.R
+++ EMDAT_test_utils.R
@@ -91,63 +91,6 @@ find_double_and_left_clicks <- function(internal_data.df){
     time_stamps <- subset(clicks.df, select=timestamp)$timestamp
     x_coords <- as.numeric(as.character(subset(clicks.df, select=x_coord)$x_coord))
     y_coords <- as.numeric(as.character(subset(clicks.df, select=y_coord)$y_coord))
-
-    is_double_click <- TRUE
-    double_click_count <- 0
-    left_click_count <- 1 # first left click is not counted in the loop
-    double_click_index <- 1000000
-    left_click_index <- 1000000
-
-    for(i in 1:(length(x_coords)-1)) {
-
-      if(is_double_click &
-         (time_stamps[i+1] - time_stamps[i]) <= 700 &
-         (x_coords[i+1] - x_coords[i]) <= 10 &
-         (y_coords[i+1] - y_coords[i]) <= 10
-      ){
-        double_click_count <- double_click_count + 1
-        left_click_count <- left_click_count - 1
-        is_double_click <- FALSE
-        if(i < double_click_index){
-          clicks[3] <- time_stamps[i+1]
-          double_click_index <- i
-        }
-      } else{
-        left_click_count <- left_click_count + 1
-        is_double_click <- TRUE
-        if(i < left_click_index){
-          clicks[4] <- time_stamps[i]
-          left_click_index <- i
-        }
-      }
-    }
-    clicks[1] <- double_click_count
-    clicks[2] <- left_click_count
-    return(clicks)
-  }
-}
-find_double_and_left_clicks <- function(internal_data.df){
-
-  clicks.df <- subset(internal_data.df, event == "LeftMouseClick")
-  clicks <- c() # c[1]: double click count, c[2]: left click count,
-  clicks[3] <- -1    # c[3]: time of first double click, with default being -1
-  clicks[4] <- -1    # c[4]: time of first left click, with default being -1
-
-  if(nrow(clicks.df) == 0){
-    clicks[1] <- 0
-    clicks[2] <- 0
-    return(clicks)
-
-  } else if(nrow(clicks.df) == 1){
-    clicks[1] <- 0
-    clicks[2] <- 1
-    clicks[4] <- subset(clicks.df, select=timestamp)[1,]
-    return(clicks)
-
-  } else{
-    time_stamps <- subset(clicks.df, select=timestamp)$timestamp
-    x_coords <- as.numeric(as.character(subset(clicks.df, select=x_coord)$x_coord))
-    y_coords <- as.numeric(as.character(subset(clicks.df, select=y_coord)$y_coord))
     
     # "D for double and "L" for left. Keep track of double and left clicks
     #  Used for determining the first double and left
@@ -197,49 +140,5 @@ find_double_and_left_clicks <- function(internal_data.df){
   }
 }
 
-# find_double_and_left_click_rates <- function(internal_data.df, length){
-#   
-#   click_rate.df <- subset(internal_data.df, event == "LeftMouseClick")
-#   click_rates <- c() # c[1]: double click rate, c[2]: left click rate
-#   
-#   if(nrow(click_rate.df) == 0){
-#     click_rates[1] <- 0
-#     click_rates[2] <- 0
-#     return(click_rates)
-#     
-#   } else if(nrow(click_rate.df) == 1){
-#     click_rates[1] <- 0
-#     click_rates[2] <- 1
-#     return(click_rates)
-#     
-#   } else{
-#     time_stamps <- subset(click_rate.df, select=timestamp)$timestamp
-#     x_coords <- as.numeric(as.character(subset(click_rate.df, select=x_coord)$x_coord))
-#     y_coords <- as.numeric(as.character(subset(click_rate.df, select=y_coord)$y_coord))
-#     
-#     is_double_click <- TRUE
-#     double_click_count <- 0
-#     left_click_count <- 1 # first left click is not counted in the loop
-#     
-#     for(i in 1:(length(x_coords)-1)) {
-#       
-#       if(is_double_click & 
-#          (time_stamps[i+1] - time_stamps[i]) <= 700 & 
-#          (x_coords[i+1] - x_coords[i]) <= 10 &
-#          (y_coords[i+1] - y_coords[i]) <= 10
-#       ){
-#         double_click_count <- double_click_count + 1
-#         left_click_count <- left_click_count - 1
-#         is_double_click <- FALSE
-#       } else{
-#         left_click_count <- left_click_count + 1
-#         is_double_click <- TRUE
-#       }
-#     } 
-#     click_rates[1] <- double_click_count/length
-#     click_rates[2] <- left_click_count/length
-#     return(click_rates)
-#   }
-# }
 
 
