92414f6685b408bdaff57726509eb740ef764f09
uta <uta>
2017-07-05 11:26:12 -0700

    temp save

--- EMDAT_propSaccade3P.R
+++ EMDAT_propSaccade3P.R
@@ -3,7 +3,7 @@ source("EMDAT_testUtils.R")
 
 root <- "Part2_EMDATInternal_EMDATOutput/three_parts_study_data/"
 export_files_root <- "Part1_TobiiV3Output_EMDATInternal/three_parts_study_data/seg_and_export/P"
-internal_data_files_path <- paste(root, "EMDAT_internal_data/", sep = "")
+internal_data_files_path <- paste(root, "EMDAT_int_data_saccade_param/", sep = "")
 seg_files_path <- paste(root, "seg_files/P", sep = "")
 
 cumulative_counter <- 0
@@ -20,11 +20,12 @@ test_param <- function(participant, seg_file, last_participant){
   scene.names <- unique(seg_file.df[,"scene"])
   
   # reads in the needed internal EMDAT data file once for the given participant 
-  saccade_data.df <- read.csv(paste(internal_data_files_path,"EMDATdata_sac_P", participant, ".tsv", sep=""), sep="\t")
+  saccade_data.df <- read.csv(paste(internal_data_files_path, "EMDATinternaldata_saccades_", participant, ".csv", sep=""), sep=",")
   
   # reads in tobii_export file for the participant and process it for all saccade data     
   tobii_export.df <- read.csv(paste(export_files_root, participant, "_Data_Export.tsv", sep = ""), sep="\t")
-  ######## TODO1: put logic to extract all saccaade data here #############
+  
+  # extract all saccaade data
   in_saccade <- FALSE
   in_fixation <- FALSE
   last_gaze_time <- 0
@@ -39,8 +40,6 @@ test_param <- function(participant, seg_file, last_participant){
   tobii_all.df <- subset(tobii_export.df, 
                          MediaName == 'ScreenRec'&
                          !is.na(RecordingTimestamp))
-  
-  # needs only the rows after the first fixation  
   
   for(i in 1:nrow(tobii_all.df)){
     
@@ -127,9 +126,6 @@ test_param <- function(participant, seg_file, last_participant){
     if(!is.na(current_row$GazePointX..ADCSpx.) &
        !is.na(current_row$GazePointY..ADCSpx.)){
       
-      if(current_row$RecordingTimestamp == 100743) {
-        print(current_row$RecordingTimestamp)
-      }
       last_gaze_time_temp <- current_row$RecordingTimestamp
       last_valid <- current_row$ValidityLeft < 2 | current_row$ValidityRight < 2 
     } else if(current_row$GazeEventType == "Fixation" &
@@ -141,32 +137,22 @@ test_param <- function(participant, seg_file, last_participant){
     }
   }
   
+  # test matchs 
   for (a_scene in scene.names) {
     
     # extracts segments within a given scene
     segment.names <- unique(subset(seg_file.df, scene==a_scene)[,"segment"])
     
-    saccade_data_scene.df <- subset(saccade_data.df, grepl(a_scene, scene) & !grepl(participant, scene))
+    saccade_data_scene.df <- subset(saccade_data.df, grepl(a_scene, scene))
     
     for(seg in segment.names){
       
-      ########## TODO2: 
-      #########       1. extract seg saccade data from all saccade data
-      #########       2. get the corresponding saccade data from the internal state file
-      #########       3. compare to show they are the same in size
       start_time <- subset(seg_file.df, segment == seg)$start
       end_time <- subset(seg_file.df, segment == seg)$end
       time_stamps_seg <- subset(time_stamps,
                              (time_stamps >= start_time) & (time_stamps <= end_time))
       
       seg_saccade <- subset(saccade_data_scene.df, grepl(seg, scene))
-      
-      # for(i in 1: length(time_stamps_seg)){
-      # 
-      #   if(time_stamps_seg[i] != seg_saccade$timestamp[i]){
-      #     print(paste(i, " ", time_stamps_seg[i]))
-      #   }
-      # }
       
       verify_equivalence(length(time_stamps_seg), nrow(seg_saccade), participant, seg, "saccade numbers", " and seg:")
     }
